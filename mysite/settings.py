21b6af8a99182e89e3d36d85f59a7a7a9001a6ad
ararbi90 <ararbi90@hotmail.com>
2020-12-31 22:33:57 -0800

    Updated files for heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '8n$vni(e2tn&aqj&o!b^s9kd1n70bskz82d$)5zgw_bxy098+7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["cache-api-portfolio.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
