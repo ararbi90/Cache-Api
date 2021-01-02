ea6a15589ed7c89e3234f54f07610212c43d19a2
ararbi90 <ararbi90@hotmail.com>
2021-01-01 22:10:21 -0800

    Updated table for logging

--- cache_api/models.py
+++ cache_api/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here.
 class Log(models.Model):
     user_name = models.CharField(max_length=200)
+    method = models.CharField(max_length=200, default='na')
     pub_date = models.DateTimeField('date published')
--- cache_api/views.py
+++ cache_api/views.py
@@ -6,25 +6,17 @@ from django.views.decorators.csrf import csrf_exempt
 from .models import Log
 from .cache.cache_set import CacheSet
 
-# Create cache
-cache = CacheSet(500)
-users = ["ahmed", "danny", "jon", "sam", "harry", "ron"];
-for user in users:
-    for i in range(10):
-        cache.add(user, str(i), str(i))
-
 # Create your views here.
 def index(request):
     return render(request, "html/index.html")
 
 def get(request):
     if request.method == "GET":
-        print(request.GET)
         if "user" not in request.GET or "key" not in request.GET:
             return JsonResponse({"error":"incorrect request"})
         user = request.GET["user"]
         key = request.GET["key"]
-        l = Log(user_name=user,pub_date=timezone.now())
+        l = Log(user_name=user,pub_date=timezone.now(), method=request.method)
         l.save()
         value = cache.get(user, key)
         return JsonResponse({"user":user, "key":key, "value":value}) 
@@ -39,9 +31,8 @@ def add(request):
         user = request.POST["user"]
         key = request.POST["key"]
         value = request.POST["value"]
-        l = Log(user_name=user,pub_date=timezone.now())
+        l = Log(user_name=user,pub_date=timezone.now(), method=request.method)
         l.save()
-        print(value)
         cache.add(user, key, value)
         return JsonResponse({"user":user, "key":key, "value":value}) 
     else:
